3b456ce564259dbcecac15977092eeceb12fe840
raymundo resendez` <rayjorg@gmail.com>
2016-09-24 13:11:33 -0700

    Started Test for reporte

--- app/src/main/java/com/example/erik/safetrip/InformacionFragment.java
+++ app/src/main/java/com/example/erik/safetrip/InformacionFragment.java
@@ -8,13 +8,21 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class InformacionFragment extends Fragment {
-
+    private Spinner spinnerSiniestro;
+    private EditText fecha;
+    private EditText hora;
+    private EditText comentarios;
+    private Button btnEditarHora;
+    private Button btnEditarFecha;
+    private Button btnMandarReporte;
 
     public InformacionFragment() {
         // Required empty public constructor
@@ -25,8 +33,29 @@ public class InformacionFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+
+
         return inflater.inflate(R.layout.fragment_informacion, container, false);
     }
 
+    @Override
+    public void onStart() {
+        super.onStart();
+        View view = getView();
 
+        spinnerSiniestro = (Spinner) view.findViewById(R.id.spinnerSiniestro);
+        fecha = (EditText) view.findViewById(R.id.etFecha);
+        hora = (EditText) view.findViewById(R.id.etHora);
+        comentarios = (EditText) view.findViewById(R.id.etComentarios);
+        btnEditarHora = (Button) view.findViewById(R.id.btn_edit_fecha);
+        btnEditarFecha = (Button) view.findViewById(R.id.btn_edit_fecha);
+        btnMandarReporte = (Button) view.findViewById(R.id.btnMandarReporte);
+
+        btnEditarFecha.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
 }
