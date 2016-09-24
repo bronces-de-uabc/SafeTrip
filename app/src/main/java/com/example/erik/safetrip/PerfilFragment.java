005a77a424133cdc7a667b0907b0de43cc9ff44f
pbrios <riosp38@uabc.edu.mx>
2016-09-24 11:57:09 -0700

    Contactos Display

--- app/src/main/java/com/example/erik/safetrip/PerfilFragment.java
+++ app/src/main/java/com/example/erik/safetrip/PerfilFragment.java
@@ -1,18 +1,19 @@
 package com.example.erik.safetrip;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class PerfilFragment extends Fragment {
-
 
     public PerfilFragment() {
         // Required empty public constructor
@@ -24,6 +25,18 @@ public class PerfilFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_perfil, container, false);
+    }
+    public void onStart(){
+        super.onStart();
+        View view = getView();
+        Button btn = (Button)view.findViewById(R.id.bt);
+        btn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public  void onClick(View v){
+                Intent inte = new Intent(getActivity(),ContentsPro.class);
+                startActivity(inte);
+            }
+        });
     }
 
 }
